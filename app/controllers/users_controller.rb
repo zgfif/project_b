d441faa799d4b4b7d407f9602e56391d7c90b5f8
zgfif <zihzag@gmail.com>
2018-03-04 13:42:19 +0200

    users were configured according admin rights

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,5 @@
-class UsersController < ApplicationController
-	def index
-		@users = User.all
-	end
-	
+class UsersController < ApplicationController	
 	def show
 		@user = User.find(params[:id])
-	end
-	def edit
-		@user = User.find(params[:id])
-	end
-	def update
-		@user = User.find(params[:id])
-
-		if @user.update(user_params)
-
-			redirect_to user_path
-		else 
-			render 'edit'
-		end
-	end
-
-	private
-
-	def user_params
-		params.require(:user).permit(:firstname, :lastname, :phone)
 	end
 end
