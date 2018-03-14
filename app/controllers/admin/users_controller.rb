3521d7f6a8ed7b192c994e5685f617fb02a566cc
zgfif <zihzag@gmail.com>
2018-03-14 21:18:41 +0200

    set confirmation for development

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,13 @@ class Admin::UsersController < Admin::AdminController
 	def index
 		@users = User.all
 	end
+	def destroy
+		@user = User.find(params[:id])
+
+		if @user.destroy
+			redirect_to admin_users_path, alert: 'User was successfully desroyed!'
+		# else 
+			# render: 'admin_index', alert: 'Can not destroy this user'
+		end
+	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,42 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   user_name:      ENV['SENDMAIL_USERNAME'],
+  #   password:       ENV['SENDMAIL_PASSWORD'],
+  #   domain:         ENV['MAIL_HOST'],
+  #   address:       'smtp.gmail.com',
+  #   port:          '587',
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
+
+  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 } #booksget.herokuapp.com 
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] } 
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = true  
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {  
+          address: "smtp.gmail.com",
+          port: 587,
+          domain: "gmail.com",
+          authentication: "plain",
+          enable_starttls_auto: true,      
+          user_name: ENV["SENDMAIL_USERNAME"],    
+          password: ENV["SENDMAIL_PASSWORD"],
+        }
+
+
+  
 end
