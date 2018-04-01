93f92ec0a2504baae40e02cfaeb9e0913e21076b
zgfif <zihzag@gmail.com>
2018-04-01 21:42:02 +0300

    set footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   root 'welcome#index'
-  get "/pages/:page" => "pages#show"
+  get "/pages/*page" => "pages#show"
 
 
   
