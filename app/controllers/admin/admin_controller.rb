e4ffcd867fed4e87a4ba704bddfd66734e94b282
zgfif <zihzag@gmail.com>
2018-04-12 20:40:48 +0300

    debug error of unsigned users

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,10 +1,11 @@
 class Admin::AdminController < ApplicationController
 	before_action :authenticate_user!
-	before_action :check_admin
+	before_action :check_admin , if: :user_signed_in?
 
 	protected
 
 	def check_admin
 		redirect_to root_path, alert: 'У Вас нет доступа к этой странице' unless current_user.admin?
 	end
+
 end
