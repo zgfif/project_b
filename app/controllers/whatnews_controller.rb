91a066e8d367d666df12d1189d06278af2f7f50b
zgfif <zihzag@gmail.com>
2018-05-09 17:14:21 +0300

    redesign of news pages

--- app/controllers/whatnews_controller.rb
+++ app/controllers/whatnews_controller.rb
@@ -1,7 +1,7 @@
 class WhatnewsController < ApplicationController
   # before_action :authenticate_user!, except: [:index, :show]
   def index
-    @whatnews = Whatnew.all
+    @whatnews = Whatnew.all.order(created_at: :desc)
   end
 
   def show
